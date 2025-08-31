b5dee79010a43dfe1539830240ce2be69e68e627
Eddy Ckedar <eddykedar@gmail.com>
2025-08-31 14:26:05 +0200

    Update index.tsx

--- prodev-mobile-app-0x01/app/index.tsx
+++ prodev-mobile-app-0x01/app/index.tsx
@@ -1,36 +1,42 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
-export default function App() {
+export default function Index() {
   return (
     <View style={styles.container}>
-     <Text>Entry Screen - Awesome</Text>
-     <Text style={styles.largeText}>Typescript is great if you practice more</Text>\r\n 
-     <Text style={styles.mediumText}>React Native provides you a single codebase for cross platforms</Text>\r\n 
-     <Text style={styles.smallText}>ALX is awesome</Text>
+      <Text>Entry Screen - Awesome</Text>
+      
+      <View>
+        <Text style={styles.largeText}>Typescript is great if you practice more</Text>
+        <Text style={styles.mediumText}>React Native provides you a single codebase for cross platforms</Text>
+        <Text style={styles.smallText}>ALX is awesome</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#fff',
+    backgroundColor: "#90caf9",
   },
   largeText: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginTop: 10,
+    fontSize: 30,
+    color: "#f44336",
+    marginBottom: 5,
+    fontWeight: "700",
+    fontVariant: ["small-caps"],
   },
   mediumText: {
-    fontSize: 18,
-    marginTop: 10,
+    fontSize: 20,
+    color: "#9c27b0",
+    marginBottom: 10,
+    fontWeight: "500",
+    textAlign: "right",
   },
   smallText: {
-    fontSize: 14,
-    fontStyle: 'italic',
-    marginTop: 10,
+    fontSize: 15,
+    color: "#2196f3",
+    fontWeight: "400",
+    textAlign: "center",
   },
 });
